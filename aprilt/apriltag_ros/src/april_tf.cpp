8b2f5c3d15aea13d577bc4fedc34e1f438860ca8
zengshiyuan <3268463239@qq.com>
2023-09-21 20:22:07 +0800

    update

--- aprilt/apriltag_ros/src/april_tf.cpp
+++ aprilt/apriltag_ros/src/april_tf.cpp
@@ -3,6 +3,8 @@
 #include <geometry_msgs/Twist.h>
 #include "apriltag_ros/ApriltagCenter.h"
 
+
+
 class PID
 {
   public:
@@ -79,7 +81,13 @@ void CenterCB(const apriltag_ros::ApriltagCenter::ConstPtr & center_msg)
     pid_lin.Ki = 0.0;
     pid_lin.Kd = 0.0;
 
+    pid_x.Kp = 0.3;
+    pid_x.Ki = 0.0001;
+    pid_x.Kd = 0.0001;
     
+    pid_yaw.Kp = 0.1;
+    pid_yaw.Ki = 0.0001; 
+    pid_yaw.Kd = 0.0001;
 
     int center_x = center_msg->center_x;
     int center_y = center_msg->center_y;
@@ -90,8 +98,8 @@ void CenterCB(const apriltag_ros::ApriltagCenter::ConstPtr & center_msg)
     tf::StampedTransform  transform;    //建立变换矩阵
     try
     {
-        listener.waitForTransform("/tag_1","/camera_link",ros::Time(0),ros::Duration(1));
-        listener.lookupTransform("/tag_1","/camera_link",ros::Time(0),transform);
+        listener.waitForTransform("/tag_1","/base_link",ros::Time(0),ros::Duration(1));
+        listener.lookupTransform("/tag_1","/base_link",ros::Time(0),transform);
     }
     catch(tf::TransformException &ex)   
     {
@@ -99,106 +107,119 @@ void CenterCB(const apriltag_ros::ApriltagCenter::ConstPtr & center_msg)
         ros::Duration(1.0).sleep();
     }
 
-    double x = transform.getOrigin().getX() /2;
-    double z = transform.getOrigin().getZ() /2;
-    double w = transform.getRotation().getW() /2;
-    // double yaw = tf::getYaw(transform.getRotation());
-    std::cout<<"x:  "<<x<<std::endl;
-    std::cout<<"z:  "<<z<<std::endl;
+    double bias = transform.getOrigin().getX() -0.2;
+    double dist = transform.getOrigin().getZ();
+    double w = transform.getRotation().getW();
+    double yaw = tf::getYaw(transform.getRotation());
+
+    double R_w = transform.getRotation().getW();
+    double R_x = transform.getRotation().getX();
+    double R_y = transform.getRotation().getY();
+    double R_z = transform.getRotation().getZ();
+    tf::Quaternion quat;
+    quat.setW(R_w);
+    quat.setX(R_x);
+    quat.setY(R_y);
+    quat.setZ(R_z);
+
+     
+      double roll, pitch, yaw;//定义存储r\p\y的容器
+      tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);//进行转换
+
+    std::cout<<"x:  "<<bias<<std::endl;
+    std::cout<<"z:  "<<dist<<std::endl;
     std::cout<<"w:  "<<w<<std::endl;
-    // std::cout<<"yaw:"<<yaw<<std::endl;
+    std::cout<<"yaw:"<<yaw<<std::endl;
     
     float angular;
     float L;   //
-    L = 0.7 * x + 0.3 * (-w);
+    L = 0.7 * bias + 0.3 * (-w);
 
     float x_ang,yaw_ang;
 
-    pid_x.Kp = 0.5;
-    pid_x.Ki = 0.0001;
-    pid_x.Kd = 0.0001;
-    
-    pid_yaw.Kp = 0.5;
-    pid_yaw.Ki = 0.0001; 
-    pid_yaw.Kd = 0.0001;
-
     //小车在左，图像在左，向左转弯
-    if ((x>-0.000)&&((center_x<=650)))
+    if ((bias>0)&&((center_x<=640)))
     {
-        if ((center_x<=400))
+        if ((center_x<=310))
         {
-            pid_x.SetTargetPoint(x);
+            pid_x.SetTargetPoint(bias);
             x_ang = pid_x.Run(x_ang,1);
-            pid_yaw.SetTargetPoint((-w));
-            yaw_ang = pid_yaw.Run(yaw_ang,1);
+            // pid_yaw.SetTargetPoint((-w));
+            // yaw_ang = pid_yaw.Run(yaw_ang,1);
             std::cout<<"x_ang:"<<x_ang<<std::endl;
-            std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
-            angular = 0.1*x_ang+ 0.9* yaw_ang;
-            // angular = x_ang*0.8;
+            // std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
+            // angular = 0.5*x_ang+ 0.5* yaw_ang;
+            angular = x_ang*1.2;
             vel_msg.angular.z = angular;
-            vel_msg.linear.x = (z-0.2);        
+            vel_msg.linear.x = (dist-0.4)*0.3;        
         }
-        else if((center_x >400))
+        else if((center_x >310))
         {
-            pid_x.SetTargetPoint(x);
+            pid_x.SetTargetPoint(bias);
             x_ang = pid_x.Run(x_ang,1);
-            pid_yaw.SetTargetPoint((-w));
-            yaw_ang = pid_yaw.Run(yaw_ang,1);
-            std::cout<<"x_ang:"<<x_ang<<std::endl;
-            std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
-            angular = 0.4*x_ang+ 0.6* yaw_ang;
-            // angular = x_ang*0.8;
+            // pid_yaw.SetTargetPoint((-w));
+            // yaw_ang = pid_yaw.Run(yaw_ang,1);
+            // std::cout<<"x_ang:"<<x_ang<<std::endl;
+            // std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
+            // angular = 0.5*x_ang+ 0.5* yaw_ang;
+            angular = x_ang*0.8;
             
             vel_msg.angular.z = -angular;
-            vel_msg.linear.x = (z-0.2);
+            vel_msg.linear.x = (dist-0.4)*0.3;
         }
     }
     //小车在左，图像在右，向右转弯
-    else if ((x>0.005) && (center_x>650))
+    else if ((bias>0.000) && (center_x>640))
     {
-        vel_msg.angular.z =  0.02*(630-center_x);
+        vel_msg.angular.z =  0.002*(640-center_x);
         vel_msg.linear.x = 0;
     }
     //小车在右，图像在右，向左转弯
-    else if ((x<0.000) && (center_x>=630))
+    else if ((bias<0) && (center_x>=640))
     {
-        if ((center_x<=880))
+        if ((center_x<=970))
         {
-            pid_x.SetTargetPoint(x);
+            pid_x.SetTargetPoint(bias);
             x_ang = pid_x.Run(x_ang,1);
-            pid_yaw.SetTargetPoint((-w));
-            yaw_ang = pid_yaw.Run(yaw_ang,1);
-            std::cout<<"x_ang:"<<x_ang<<std::endl;
-            std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
-            angular = 0.4*x_ang+ 0.6* yaw_ang;
+            // pid_yaw.SetTargetPoint((-w));
+            // yaw_ang = pid_yaw.Run(yaw_ang,1);
+            // std::cout<<"x_ang:"<<x_ang<<std::endl;
+            // std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
+            // angular = 0.5*x_ang+ 0.5* yaw_ang;
 
-            // angular = x_ang*0.8;
+            angular = x_ang*0.8;
             vel_msg.angular.z = -angular;
-            vel_msg.linear.x = (z-0.2);        
+            vel_msg.linear.x = (dist-0.4) *0.3;        
         }
-        else if((center_x >880))
+        else if((center_x >970))
         {
-            pid_x.SetTargetPoint(x);
+            pid_x.SetTargetPoint(bias);
             x_ang = pid_x.Run(x_ang,1);
-            pid_yaw.SetTargetPoint((-w));
-            yaw_ang = pid_yaw.Run(yaw_ang,1);
-            std::cout<<"x_ang:"<<x_ang<<std::endl;
-            std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
+            // pid_yaw.SetTargetPoint((-w));
+            // yaw_ang = pid_yaw.Run(yaw_ang,1);
+            // std::cout<<"x_ang:"<<x_ang<<std::endl;
+            // std::cout<<"yaw_ang:"<<yaw_ang<<std::endl;
         
-            angular = 0.1*x_ang+ 0.9* yaw_ang;
-            // angular = x_ang*0.8;
+            // angular = 0.5*x_ang+ 0.5* yaw_ang;
+            angular = x_ang*1.2;
 
             vel_msg.angular.z = angular;
-            vel_msg.linear.x = (z-0.2);
+            vel_msg.linear.x = (dist-0.4)*0.3;
         }
     }
     //小车在右，图像在左，向左转弯
-    else if ((x<-0.005) && (center_x<630))
+    else if ((bias<0.000) && (center_x<640))
     {
-        vel_msg.angular.z = 0.02*(630-center_x);
+        vel_msg.angular.z = 0.002*(640-center_x);
         vel_msg.linear.x = 0;
     }
+
+    if ((bias == 0.00))
+    {
+        vel_msg.angular.z = -w;
+    }
     
+
     // else
     // {
     //     pid_x.SetTargetPoint(x);
@@ -212,7 +233,6 @@ void CenterCB(const apriltag_ros::ApriltagCenter::ConstPtr & center_msg)
     //     vel_msg.angular.z = angular;
     //     vel_msg.linear.x = (z-0.2) * 0.25;
     // }
-
     
     if (vel_msg.linear.x>0.1)
         vel_msg.linear.x = 0.1;
@@ -236,7 +256,7 @@ int main(int argc, char * argv[])
 {
     ros::init(argc, argv, "tf_listent_node");
     ros ::NodeHandle nh;
-
+    
     vel_ = nh.advertise<geometry_msgs::Twist>("cmd_vel",10);
     ros::Subscriber tag_center_sub_  = nh.subscribe<apriltag_ros::ApriltagCenter>("/apriltag_ros_continuous_node/tag_center",1,&CenterCB);
     ros::Rate rate(30);
